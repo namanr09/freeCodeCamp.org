94382b50dae8c46adb60f5bdff7c24ef6b8717a1
naman <rohitasnaman09@gmail.com>
2025-06-29 19:31:27 +0530

    first commit

---
+++ test.kt
@@ -0,0 +1,6 @@
+fun main(){
+    // main funtion fun is the keaword to define the  function in Kotlin
+    // main is entry point of the program
+    // {} -> defines the scope of the function
+    println("knock knock nugs!")
+}
